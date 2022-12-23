6de69f3e2747b5888f14846901d24838541c87d7
codecritik <aloha@codecritik.com>
2022-12-23 12:53:31 +0100

    testing with component annotation

--- src/main/java/com/cobo/component/RestTemplateContext.java
+++ src/main/java/com/cobo/component/RestTemplateContext.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class RestTemplateContext {
 
     @Bean
